0bf663dcbe62a3e127ab1ab94b26d71d617d2328
Unibague <g3@unibague.edu.co>
2026-01-26 09:58:20 -0500

    fix producer reports

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(cors({
     }
     return callback(null, true);
   },
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   credentials: true
 }));
 
