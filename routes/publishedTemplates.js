256bbe698ac99e07ae2b7187954bd1a492c817b9
Unibague <g3@unibague.edu.co>
2026-08-13 09:19:28 -0500

    uplaod changes

--- routes/publishedTemplates.js
+++ routes/publishedTemplates.js
@@ -4,6 +4,7 @@ const controller = require('../controllers/publishedTemplates');
 router.post("/publish", controller.publishTemplate);
 
 router.delete("/delete", controller.deletePublishedTemplate);
+router.delete("/deleteLoadedData", controller.deleteAllLoadedData);
 
 router.get("/", controller.getAssignedTemplatesToProductor);
 router.get("/available", controller.getAvailableTemplatesToProductor);
